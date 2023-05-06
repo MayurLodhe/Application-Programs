15228edaf1af84e0d11e15138a65d2b9b3324789
MayurLodhe <132749363+MayurLodhe@users.noreply.github.com>
2023-05-06 16:08:12 +0530

    C_Programs

---
+++ Demo.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+
+    void Display()
+    {
+        printf("Jay Ganesh...\n");
+    }
+
+    int main()                              
+    {
+        printf("Inside main.\n");
+
+        Display();  // Function Call
+
+        return 0;
+    } 
---
+++ First.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...\n");
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int A = 11;             // Normal Glass     20 
+
+
+
+short int B = 11;       // Half glass       15
+
+
+
+long int C = 11;        // Jumbo glass      30
